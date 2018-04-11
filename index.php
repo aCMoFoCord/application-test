00aa47a99213e6df30fe4f46aa42c63149846c79
Chad Laughland <mofo@Chads-MacBook-Pro.local>
2018-04-11 11:50:20 -0400

    php classes no angular

--- index.php
+++ index.php
@@ -1,29 +1,88 @@
+<?php
+
+class Person {
+	public $id = 0;
+	public $first_name = '';
+	public $last_name = '';
+	public $email = '';
+
+	public function __construct($person){
+		$this->id = (Int)$person['id'];
+		$this->first_name = (String)$person['first_name'];
+		$this->last_name = (String)$person['last_name'];
+		$this->email = (String)$person['email'];
+	}
+}
+
+class People {
+
+	public $persons = [];
+
+	public function __construct($people){
+		foreach($people as $person){
+			$this->persons[] = New Person($person);
+		}
+	}
+}
+
+$people = array(
+   array('id'=>1, 'first_name'=>'John', 'last_name'=>'Smith', 'email'=>'john.smith@hotmail.com'),
+   array('id'=>2, 'first_name'=>'Paul', 'last_name'=>'Allen', 'email'=>'paul.allen@microsoft.com'),
+   array('id'=>3, 'first_name'=>'James', 'last_name'=>'Johnston', 'email'=>'james.johnston@gmail.com'),
+   array('id'=>4, 'first_name'=>'Steve', 'last_name'=>'Buscemi', 'email'=>'steve.buscemi@yahoo.com'),
+   array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
+);
+
+$People = new People($people);
+?>
+
 <html ng-app='eBaseApp'>
 <head>
 	<!doctype html>
-	<title>eBASE Developer Application Test</title>
-	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
-	<script src="app.js"></script>
-	<link rel='stylesheet' href='style.css' />
+	<title>eBASE Developer Application Test - Alternate</title>
+	<style>
+	body {
+		display:flex;
+		flex-direction:column;
+		font-family: Helvetica, Arial, 'sans-serif';
+	}
+	.container {
+		width:100%;
+		max-width:600px;
+		margin:20px auto;
+	}
+	table,
+	th,
+	td {
+		border:1px solid black;
+		border-collapse:collapse;
+		padding:5px;
+	}
+	</style>
 </head>
 <body ng-controller='mainCtrl as mCtrl'>
 	
 	<div class='container'>
-		<h1>People</h1>
-		<div ng-show='mCtrl.loading' class='loader'>Loading ...</div>
-		<table ng-hide='mCtrl.loading'>
+		<table>
 			<tr>
 				<th>First Name</th>
 				<th>Last Name</th>
 				<th>Email Address</th>
 				<th>Alert</th>
 			</tr>
-			<tr ng-repeat='p in mCtrl.people'>
-				<td>{{::p.first_name}}</td>
-				<td>{{::p.last_name}}</td>
-				<td>{{::p.email}}</td>
-				<td><button type='button' ng-click='mCtrl.alertPerson(p);'>Alert!</button></td>
+			<?php
+			foreach($People->persons as $person){
+				$alertText = $person->first_name.' '.$person->last_name.' - '.$person->email;
+			?>
+			<tr>
+				<td><?php echo $person->first_name; ?></td>
+				<td><?php echo $person->last_name; ?></td>
+				<td><?php echo $person->email; ?></td>
+				<td><button type='button' onclick="alert('<?php echo $alertText; ?>');"">Alert!</button></td>
 			</tr>
+			<?php
+			}
+			?>
 		</table>
 	</div>
 
